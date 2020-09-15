aa577a02b5cdf1138ac3b24c26ba514a511f395b
AShaver43 <gofrodude_197@hotmail.com>
2020-09-15 14:26:04 -0700

    added Auth service for login and register

---
+++ src/Service/AuthService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const url = `https://codedeckapi.azurewebsites.net/api/auth`
+
+class AuthService {
+    static login(username, password) {
+        return axios.post(`${url}/Login`, {username, password})
+    }
+
+    static register(username, password) {
+        return axios.post(`${url}/Register`, {username, passowrd})
+    }
+
+    static getCurrentUser() {
+        return JSON.parse(sessionStorage.getItem("user"))
+    }
+}
+
+export default AuthService

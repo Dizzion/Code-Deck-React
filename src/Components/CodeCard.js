c1a6ad479ffd483724f749a8c96a22e729d788d0
AShaver43 <gofrodude_197@hotmail.com>
2020-09-15 19:41:49 -0700

    added Code card component

---
+++ src/Components/CodeCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class CodeCard extends Component {
+    render() {
+        console.log(this.props.card)
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CodeCard

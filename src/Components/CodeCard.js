3aa0080db50c5b218f82155a325ad5316f2faa47
AShaver43 <gofrodude_197@hotmail.com>
2020-09-16 23:48:19 -0700

    added Code Editor and tab ids

--- src/Components/CodeCard.js
+++ src/Components/CodeCard.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react'
 import ReactCardFlip from 'react-card-flip'
+import AceEditor from 'react-ace'
+
+import "ace-builds/src-min-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/snippets/python";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/snippets/java";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/snippets/csharp";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
 
 class CodeCard extends Component {
     state = {
@@ -12,11 +25,28 @@ class CodeCard extends Component {
     }
 
     render() {
+        console.log(this.props.cardId)
         return (
             <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
                 <div className="card">
                     <div className="card-body">
-                        <p className="card-text">{this.props.card.challenge}</p>
+                        <AceEditor 
+                            mode="javascript"
+                            theme="monokai"
+                            name="Editor"
+                            showPrintMargin={true}
+                            showGutter={true}
+                            highlightActiveLine={true}
+                            value={
+`${this.props.card.challenge}`
+}
+                            setOptions={{
+                                enableBasicAutocompletion: true,
+                                enableLiveAutocompletion: false,
+                                enableSnippets: true,
+                                showLineNumbers: true,
+                                tabSize: 4
+                            }}/>
                         <button className="btn btn-primary" onClick={this.handleClick}>Click to Flip</button>
                     </div>
                 </div>
@@ -25,25 +55,25 @@ class CodeCard extends Component {
                     <nav>
                         <ul className="nav nav-tabs" id="myTab" role="tablist">
                             <li className="nav-item" role="presentation">
-                                <a className="nav-link active" id="JavaScript-tab" data-toggle="tab" href="#JavaScript" role="tab" aria-controls="JavaScript" aria-selected="true">JavaScript</a>
+                                <a className="nav-link active" id="JavaScript-tab" data-toggle="tab" href={"#JavaScript"+this.props.cardId} role="tab" aria-controls="JavaScript" aria-selected="true">JavaScript</a>
                             </li>
                             <li className="nav-item" role="presentation">
-                                <a className="nav-link" id="Java-tab" data-toggle="tab" href="#Java" role="tab" aria-controls="Java" aria-selected="false">Java</a>
+                                <a className="nav-link" id="Java-tab" data-toggle="tab" href={"#Java"+this.props.cardId} role="tab" aria-controls="Java" aria-selected="false">Java</a>
                             </li>
                             <li className="nav-item" role="presentation">
-                                <a className="nav-link" id="Python-tab" data-toggle="tab" href="#Python" role="tab" aria-controls="Python" aria-selected="false">Python</a>
+                                <a className="nav-link" id="Python-tab" data-toggle="tab" href={"#Python"+this.props.cardId} role="tab" aria-controls="Python" aria-selected="false">Python</a>
                             </li>
                             <li className="nav-item" role="presentation">
-                                <a className="nav-link" id="C-tab" data-toggle="tab" href="#C" role="tab" aria-controls="C" aria-selected="false">C</a>
+                                <a className="nav-link" id="C-tab" data-toggle="tab" href={"#C"+this.props.cardId} role="tab" aria-controls="C" aria-selected="false">C</a>
                             </li>
                         </ul>
                     </nav>
                     <div className="card-body">
                         <div className="tab-content" id="myTabContent">
-                            <div className="tab-pane fade show active" id="JavaScript" role="tabpanel" aria-labelledby="JavaScript-tab"><p className="card-text">{this.props.card.javaScriptAnswer}</p></div>
-                            <div className="tab-pane fade" id="Java" role="tabpanel" aria-labelledby="Java-tab"><p className="card-text">{this.props.card.javaAnswer}</p></div>
-                            <div className="tab-pane fade" id="Python" role="tabpanel" aria-labelledby="Python-tab"><p className="card-text">{this.props.card.pythonAnswer}</p></div>
-                            <div className="tab-pane fade" id="C" role="tabpanel" aria-labelledby="C-tab"><p className="card-text">{this.props.card.cAnswer}</p></div>
+                            <div className="tab-pane fade show active" id={"#JavaScript"+(this.props.cardId)} role="tabpanel" aria-labelledby="JavaScript-tab"><p className="card-text">{this.props.card.javaScriptAnswer}</p></div>
+                            <div className="tab-pane fade" id={"Java"+this.props.cardId} role="tabpanel" aria-labelledby="Java-tab"><p className="card-text">{this.props.card.javaAnswer}</p></div>
+                            <div className="tab-pane fade" id={"Python"+this.props.cardId} role="tabpanel" aria-labelledby="Python-tab"><p className="card-text">{this.props.card.pythonAnswer}</p></div>
+                            <div className="tab-pane fade" id={"C"+this.props.cardId} role="tabpanel" aria-labelledby="C-tab"><p className="card-text">{this.props.card.cAnswers}</p></div>
                         </div>
 
                         <button className="btn btn-secondary" onClick={this.handleClick}>Click to Flip</button>

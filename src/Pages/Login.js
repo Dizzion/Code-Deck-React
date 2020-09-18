db35f5b025812e536635e8910870177b59a2ab20
AShaver43 <gofrodude_197@hotmail.com>
2020-09-17 21:48:14 -0700

    added login page for users

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import AuthService from '../Service/AuthService'
 
 class Login extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await AuthService.login(this.state.username, this.state.password)
+            .then(res => {
+                if (!res.data.token) {
+                    return false
+                }
+                let user = {
+                    token: res.data.token,
+                    username: this.state.username
+                }
+                this.props.storeUser(user)
+                this.props.history.push('/')
+            })
+            .catch(err => console.log(err))
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="container-sm">
+                <div className="body container">
+                    <form className="form-signin mt-5" onSubmit={this.handleSubmit}>
+                        <h1 className="h3 mb-3 font-weight-normal">Please Login</h1>
+                        <label htmlFor="username" className="sr-only">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            className="form-control mb-2"
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            placeholder="Username" />
+                        <label htmlFor="password" className="sr-only">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="form-control mb-2"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            placeholder="Password" />
+                        <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                    </form>
+                    <Link className="btn btn-lg btn-success btn-block mt-3" to="/Register">Register Here!</Link>
+                </div>
             </div>
         )
     }

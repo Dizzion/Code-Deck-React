9b492aee0f422f6b49f96fcdc11ba7938245159d
AShaver43 <gofrodude_197@hotmail.com>
2020-09-20 15:28:22 -0700

    postioned list for all challenges

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CodeDeckModel from '../Model/CodeDeckModel'
 import CodeCard from '../Components/CodeCard'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     state = {
@@ -20,9 +21,20 @@ class Home extends Component {
         let codeCards = this.state.CodeDeck.map((codeCard, i) => {
             return <CodeCard card={codeCard} key={i} cardId={i} />
         })
+        let quickLinks = this.state.CodeDeck.map((codeCard, i) => {
+        return <Link className="list-group-item list-group-item-action" to={`/${codeCard.cardId}`}>{codeCard.challengeTitle}</Link>
+        })
         return (
-            <div className="container offset">
-                <div className="card-deck justify-content-center mt-5 row row-cols-1 row-cols-md-1">
+            <div className="container-fluid row offset">
+                <div className="col-md-3 col-xl-2">
+                    <div className="list-group">
+                        {this.state.CodeDeck ? quickLinks: 'Loading.....'}
+                    </div>
+                </div>
+                <div className="col-md-2 col-xl-2">
+                    
+                </div>
+                <div className="card-deck col-md-6 justify-content-center mt-5 row row-cols-1 row-cols-md-1">
                     {this.state.CodeDeck ? codeCards : 'Loading.....'}
                 </div>
             </div>
